8526384cf7076197f494e60514bbdcdc294b4f2e
DangerousCoder06 <kumar.vaibhav6122005@gmail.com>
2025-07-11 16:31:29 +0530

    update CORS config

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: ["http://localhost:5173",
+        process.env.FRONTEND_URL]
     }
 })
 const port = 3000
